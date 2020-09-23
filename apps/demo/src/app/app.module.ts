c3f861c46093e7b3553976a8511ddb6afc2e7074
snehalp29 <snehalpatkar@gmail.com>
2020-09-22 20:28:32 -0400

    used shared ui component library

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedUiMaterialModule } from '@angular-material/shared/ui-material';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, SharedUiMaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
